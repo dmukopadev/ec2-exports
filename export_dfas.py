df5e3f2ced298ba904b48cbd49c67e28e6c5fda5
dmukopadev <dmukopadev@gmail.com>
2025-11-19 14:14:51 +0200

    added script to extract dfas

---
+++ export_dfas.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import mysql.connector
+
+conn = mysql.connector.connect(
+    host='localhost',
+    user='ecommerce',
+    password='3cK0mMeRc3',
+    database='ecommerce_processor'
+)
+
+query = """
+SELECT * FROM dfas
+"""
+
+# Read data into a pandas DataFrame
+df = pd.read_sql(query, conn)
+
+# Export to Excel
+df.to_excel('dfas.xlsx', index=False)
+print("File dfas.xlsx created successfully!")
